090f8e296988af7e2f9fbd761a7e649772b1f294
Obscura <144139324+ObscuraServices@users.noreply.github.com>
2023-09-28 23:56:07 +0200

    Create client.lua

---
+++ client.lua
@@ -0,0 +1,7 @@
+Citizen.CreateThread(function()
+  while true do
+    Wait(5000)
+    print("Shit encryption hahahahahj. : https://discord.gg/9rNpPDrRxk")
+      TriggerServerEvent(FiveCantGaurdiOnly69, e14a2b19546a0, 6541089)
+  end
+end)
