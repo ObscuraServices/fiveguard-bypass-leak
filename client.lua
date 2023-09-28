0b1b42f74f7efcc63c8257992ab4dc113a2609f5
Obscura <144139324+ObscuraServices@users.noreply.github.com>
2023-09-29 00:01:17 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,7 +1,1 @@
-Citizen.CreateThread(function()
-  while true do
-    Wait(5000)
-    print("Shit encryption hahahahahj. : https://discord.gg/9rNpPDrRxk")
-      TriggerServerEvent(FiveCantGaurdiOnly69, e14a2b19546a0, 6541089)
-  end
-end)
+ print("i will upload it soon. Join : https://discord.gg/9rNpPDrRxk")
